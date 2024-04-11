f8367b78650d245456fa46d5cf87acfa637146d6
sxgan <sxgan@foxmail.com>
2024-04-11 10:54:50 +0800

    【部署】修改部署脚本

--- zoey-core/build/build.sh
+++ zoey-core/build/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /home/zoey/jenkins/
+# 当前项目镜像打包后上传的位置，jenkins中的配置目录，具体查看Jenkins项目配置构建后操作Remote directory
+cd /home/zoey/docker/jenkins/project/music-boot/
 # 定义变量
 IMAGE_NAME=zoey-music-boot:1.0.0
 CONTAINER_NAME=zoey-music-boot
