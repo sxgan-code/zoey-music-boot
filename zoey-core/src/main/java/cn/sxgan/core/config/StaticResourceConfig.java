fe33863ce4aa503af7f1d1a7ca303b8bead80f9d
sxgan <sxgan@foxmail.com>
2024-04-17 19:03:24 +0800

    【部署】修改dockerfile赋值资源路径

--- zoey-core/src/main/java/cn/sxgan/core/config/StaticResourceConfig.java
+++ zoey-core/src/main/java/cn/sxgan/core/config/StaticResourceConfig.java
@@ -15,7 +15,7 @@ public class StaticResourceConfig {
             @Override
             public void addResourceHandlers(ResourceHandlerRegistry registry) {
                 String userDir = System.getProperty("user.dir");
-                String fileUrl = "file:" + userDir + "/";
+                String fileUrl = userDir + "/resources";
                 log.info("当前项目根目录为：{}", fileUrl);
                 registry.addResourceHandler("/static/**").addResourceLocations(fileUrl);
             }
