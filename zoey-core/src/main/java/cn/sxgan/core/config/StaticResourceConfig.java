aec21893ed7551ac5ecc2c12532ab8e5fe380ac9
zoey <sxgan@foxmail.com>
2024-04-17 22:10:04 +0800

    【部署】修改linux下静态资源访问路径问题修改

--- zoey-core/src/main/java/cn/sxgan/core/config/StaticResourceConfig.java
+++ zoey-core/src/main/java/cn/sxgan/core/config/StaticResourceConfig.java
@@ -15,8 +15,9 @@ public class StaticResourceConfig {
             @Override
             public void addResourceHandlers(ResourceHandlerRegistry registry) {
                 String userDir = System.getProperty("user.dir");
-                String fileUrl = userDir +"/";
-                log.info("当前项目根目录为：{}", fileUrl);
+                log.info("当前根目录"+userDir);
+                String fileUrl = userDir +"resources/";
+                log.info("当前资源目录目录为：{}", fileUrl);
                 registry.addResourceHandler("/static/**").addResourceLocations("file:"+fileUrl);
             }
         };
